01337b4265b1c67dc20d148e38eedbb611d93859
James Burton <jimburton1@gmail.com>
2025-10-11 23:47:42 +0100

    Ciphers.

--- python/src/homework/caesar.py
+++ python/src/homework/caesar.py
@@ -1,18 +1,28 @@
+"""Some simplpe ciphers."""
 from string import ascii_lowercase, ascii_uppercase
 
 def enc_ceasar(key, text):
+    """Encode text with a Caser cipher with a given offset.
+    The encoding may include non alpha-numeric ASCII characters."""
     enc = ""
     for c in text:
         enc += chr(ord(c)+key)
     return enc
 
 def dec_ceasar(key, text):
+    """Decode a Caesar cipher with a given offset."""
     dec = ""
     for c in text:
         dec += chr(ord(c)-key)
     return dec
 
 def rot13(text):
+    """Rot13 is a Caesar cipher with key=13. This means that callibg Rot13
+    twice recovers the original text. The encoding transforms lower case
+    alphabetic characters to their Rot13 lowercase counterpart, and
+    similarly for upper case input. Non alphabetic characters are not
+    changed.
+    """
     enc = ""
     for c in text:
         c_rot = ''
